87605715509c1e89ee6f22b4c52400519b6501e5
Mushahid-H <mushahidhussain882@gmail.com>
2026-08-02 14:58:48 +0500

    API Testing: Login user with Incorrect creds

--- tests/api/Login.spec.ts
+++ tests/api/Login.spec.ts
@@ -10,4 +10,13 @@ test.describe('Login API',()=>{
         expect(data.success).toBe(true);
         expect(data).toHaveProperty('token');
     });
+    test('should fail to login a user with incorrect credentials',async({request})=>{
+        const loginAPI=new LoginAPI(request);
+        const response = await loginAPI.loginUser('test@example1234.com','WrongPassword!23');
+        expect(response.status()).toBe(400);
+        const data =await response.json();
+        expect(data.success).toBe(false);
+        expect(data.error).toBe('Invalid email or password');
+    });
+
 });
