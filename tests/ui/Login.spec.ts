a3b10ca9131b368652d9d973e5b2278a0bfd9d29
Mushahid-H <mushahidhussain882@gmail.com>
2026-08-10 10:28:49 +0500

    UI Testcase, Login with invalid password

--- tests/ui/Login.spec.ts
+++ tests/ui/Login.spec.ts
@@ -10,12 +10,20 @@ test.describe('Login Page', () => {
         await reusableFtns.goto();
         await reusableFtns.ensureLoginPage();
     });
-    test('should navigate to login page and fill the form', async ({ page }) => {
+    test('should login successfully', async ({ page }) => {
         const loginPage = new LoginPage(page);
         const reusableFtns = new ReusableFtns(page);
         await loginPage.fillLoginForm(process.env.LOGIN_EMAIL!, process.env.LOGIN_PASSWORD!);
         await loginPage.clickSubmitBtn();
         await reusableFtns.ensureHomePage();
+
+    });
+    test('should throws error if login with invalid pass', async ({page})=>{
+        const loginPage = new LoginPage(page);
+        const reusableFtns = new ReusableFtns(page);
+        await loginPage.fillLoginForm(process.env.LOGIN_EMAIL!, 'sdfghjklhgfd');
+        await loginPage.clickSubmitBtn();
+        await loginPage.ensureErrorMessageVisible();
 
     });
 
